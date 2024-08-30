b880a41d004188fd51817172e51e48eb887cdcaa
Peter Omollo <omollpeter@gmail.com>
2024-08-30 04:18:11 +0300

    Create filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,13 @@ from typing import List
 import re
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
-    """Returns log message obfuscated"""
-    for field in fields:
-        message = re.sub(rf"{re.escape(field)}=.*?{re.escape(separator)}", f"{re.escape(field)}={redaction}{re.escape(separator)}", message)
-    return message
+# def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+#     """Returns log message obfuscated"""
+#     for field in fields:
+#         message = re.sub(rf"{re.escape(field)}=.*?{re.escape(separator)}", f"{re.escape(field)}={redaction}{re.escape(separator)}", message)
+#     return message
+
+def filter_datum(fields, redaction, message, separator):
+    """Returns obfuscated message"""
+    pattern = r'({})=[^{}]*'.format('|'.join(re.escape(field) for field in fields), re.escape(separator))
+    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
