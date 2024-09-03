1e570e586f754d51fd1289cbf5ce33bcb0388bef
Peter Omollo <omollpeter@gmail.com>
2024-09-03 17:53:29 +0300

    Create BasicAuth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_type = os.getenv("AUTH_TYPE")
-if auth_type == "auth":
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Contains a class that implements Basic Authentication
+"""
+
+
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Implements Basic Authentication
+    """
+    pass
