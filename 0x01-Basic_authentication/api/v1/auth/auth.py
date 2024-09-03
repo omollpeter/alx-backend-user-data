e80e52eab7705adda6a398e0d700c9b04c1f8e54
Peter Omollo <omollpeter@gmail.com>
2024-09-03 12:55:43 +0300

    Implement a class to manage API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains Auth class
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Manages API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns False (modified later)
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns none (modified later)
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None
+        """
+        return None
