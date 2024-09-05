3f0f21d4cb2c1f6cef4ef524c4db0c3a4c63915d
Peter Omollo <omollpeter@gmail.com>
2024-09-05 15:27:43 +0300

    add a method to session that creates a session for a user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,10 +5,24 @@ Contains class for implementing session based authentication
 
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     Implements session based authentication
     """
-    pass
+
+    user_id_by_session_id =  {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates session id for user id
+        """
+        if not user_id:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
