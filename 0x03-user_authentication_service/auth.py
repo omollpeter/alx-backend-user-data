05e727e979bd9ac61fd7cf03cde1ccb400303490
Peter Omollo <omollpeter@gmail.com>
2024-09-11 05:45:40 +0300

    Fix destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,7 +97,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user_id, session_id=user.session_id)
+            self._db.update_user(user_id, session_id=None)
             return None
         except NoResultFound:
             return None
