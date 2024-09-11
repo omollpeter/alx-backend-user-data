6d600128e208a9d9b824c2d41f098260b028f073
Peter Omollo <omollpeter@gmail.com>
2024-09-11 05:57:16 +0300

    Add a method that generates user's password reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,3 +101,15 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Sets and returns a users password reset token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            user.reset_token = _generate_uuid()
+            self._db._session.commit()
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError()
